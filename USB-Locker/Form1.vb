ef91c41f3404c77b9fbef187cc2700c06419ba7a
Akira <72250044+Ak1r4Yuk1@users.noreply.github.com>
2020-10-02 11:38:47 +0200

    Version 1.0

---
+++ USB-Locker/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports Microsoft.Win32
+Public Class Form1
+
+    Private Sub BlockUSBPort()
+        Dim regkey As RegistryKey
+        regkey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\USBSTOR", True)
+        regkey.SetValue("Start", 4)
+    End Sub
+
+    Private Sub UnblockUSBPort()
+        Dim regkey As RegistryKey
+        regkey = Registry.LocalMachine.OpenSubKey("SYSTEM\CurrentControlSet\Services\USBSTOR", True)
+        regkey.SetValue("Start", 3)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        BlockUSBPort()
+        PictureBox1.Image = My.Resources.red
+
+        MsgBox("Locked")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        UnblockUSBPort()
+        PictureBox1.Image = My.Resources.green
+        MsgBox("Unlocked")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UnblockUSBPort()
+        PictureBox1.Image = My.Resources.green
+    End Sub
+End Class
